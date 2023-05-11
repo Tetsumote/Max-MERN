121160f2d0a60b7abd7553efd60c9a5e853c9871
tetsumote <unionproject88@gmail.com>
2023-05-11 17:38:50 +0200

    add custom hook

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -3,7 +3,10 @@ import { useParams } from 'react-router-dom';
 
 import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
-import { VALIDATOR_REQUIRE,VALIDATOR_MINLENGTH, VALIDATOR_MIN } from "../../shared/util/validators";
+
+import { VALIDATOR_REQUIRE,VALIDATOR_MINLENGTH } from "../../shared/util/validators";
+
+import { useForm } from "../../shared/hooks/form-hook";
 
 import './PlaceForm.css';
 
@@ -39,12 +42,28 @@ const UpdatePlace = () => {
     const placeId = useParams().placeId;
 
     const identifiedPlace = DUMMY_PLACES.find(p => p.id === placeId)
+    
+    const [formState, inputHandler] = useForm({
+        title:{
+            value: identifiedPlace.title,
+            isValid:true,
+        },
+        description:{
+            value:identifiedPlace.description,
+            isValid:true,
+        }
+    }, true)
+
+    const placeUpdateSubmitHandler = event => {
+        event.preventDefault();
+        console.log(formState.inputs);
+    }
 
     if(!identifiedPlace){
         return <div className="center"><h2>Could not find place!</h2></div>
     }
 
-    return <form className="place-form">
+    return <form className="place-form" onSubmit={placeUpdateSubmitHandler}>
         <Input
             id="title"
             element="input"
@@ -52,11 +71,9 @@ const UpdatePlace = () => {
             label="Title"
             validators={[VALIDATOR_REQUIRE()]}
             errorText="Please enter a valid title."
-            onInput={() => {
-
-            }}
-            value={identifiedPlace.title}
-            valide={true}
+            onInput={inputHandler}
+            initialValue={formState.inputs.title.value}
+            initialValid={formState.inputs.title.isValid}
         />
         <Input
             id="description"
@@ -64,13 +81,11 @@ const UpdatePlace = () => {
             label="Description"
             validators={[VALIDATOR_MINLENGTH(5)]}
             errorText="Please enter a valid description (min. 5 characters)."
-            onInput={() => {
-
-            }}
-            value={identifiedPlace.title}
-            valide={true}
+            onInput={inputHandler}
+            initialValue={formState.inputs.description.value}
+            initialValid={formState.inputs.description.isValid}
         />
-        <Button type="submit" disabled={true}>UPDATE PLACE</Button>
+        <Button type="submit" disabled={!formState.isValid}>UPDATE PLACE</Button>
     </form>
 
 }
--- src/shared/hooks/form-hook.js
+++ src/shared/hooks/form-hook.js
@@ -27,6 +27,24 @@ const formReducer = (state, action) => {
     }
 }
 
-export const useForm = () => {
+export const useForm = (initialInputs, initialFormValidity) => {
+    const [formState, dispatch] = useReducer(formReducer,{
+        inputs:initialInputs,
+        isValid:initialFormValidity
+    });
+
+    const inputHandler = useCallback((id, value, isValid) => {
+        dispatch({
+            type: 'INPUT_CHANGE',
+            value: value,
+            isValid: isValid,
+            inputId: id
+        });
+    }, [])
+
+
+
+    return [ formState, inputHandler ]
 
 }
+
